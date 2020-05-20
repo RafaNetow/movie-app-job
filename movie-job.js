4613796e78ea092976ba6bec168cce5543bd5a99
unknown <rafanetow@gmail.com>
2020-05-19 23:06:09 -0700

    adding sendGrid

--- movie-job.js
+++ movie-job.js
@@ -4,6 +4,19 @@ const coolMovies = require('./movieList.json')
 let keepRunning = true;
 let movieAppMessage = "Hello , we are happy that you are on our app, we recomend you this movies:"
 let movies = [];
+const mailgun = require("mailgun-js");
+const DOMAIN = "sandbox05185197488b4b23967712dc2b077597.mailgun.org";
+const mg = mailgun({apiKey: "690467effb55c56921b6e5812aa5ab5f-e5e67e3e-6c51f431", domain: DOMAIN});
+const data = {
+	from: "Mailgun Sandbox <postmaster@sandbox05185197488b4b23967712dc2b077597.mailgun.org>",
+	to: "rafanetow@gmail.com",
+	subject: "Hello",
+	text: "Testing some Mailgun awesomness!"
+};
+mg.messages().send(data, function (error, body) {
+	console.log(body);
+});
+
 
 function run() {
     if (keepRunning) {
@@ -19,13 +32,12 @@ function run() {
                 ///thinking
                 let url2 = 'http://www.omdbapi.com/?i=tt3896198&apikey=f29c80cb&t=lalaland';
                 movies.forEach(element =>  {
-                    console.log("......",coolMovies[element])
-                });
-                fetch(url2)
+                    fetch(url2)
                     .then(response => response.json())
                     .then(data => {
                       
             }).catch(err => console.log(err))
+                });
             })
             .catch(err => console.log(err))
         setTimeout(run, "50000");
