9ce5d9b23b6bf57283f90c906cb38556a2659b38
unknown <rafanetow@gmail.com>
2020-05-20 02:38:17 -0700

    send message to my email

--- movie-job.js
+++ movie-job.js
@@ -7,7 +7,7 @@ let movieMessage = ' ';
 const mailgun = require("mailgun-js");
 const DOMAIN = "sandbox05185197488b4b23967712dc2b077597.mailgun.org";
 const mg = mailgun({apiKey: "690467effb55c56921b6e5812aa5ab5f-e5e67e3e-6c51f431", domain: DOMAIN});
-const text = {
+let email = {
 	from: "Mailgun Sandbox <postmaster@sandbox05185197488b4b23967712dc2b077597.mailgun.org>",
 	to: "rafanetow@gmail.com",
 	subject: "Hello",
@@ -30,19 +30,36 @@ function run() {
                 console.log(movies)
                 console.log(data[1].typeOfMovies)
                 console.log(coolMovies);
-                ///thinking
+                /**Year: '2003',
+  Rated: 'N/A',
+  Released: '26 Dec 2003',
+  Runtime: '270 min',
+  Genre: 'Documentary, Music',
+  Director: 'N/A',
+  Writer: 'Robin Ince, Stewart Williams',
+  Actors: 'Denise Van Outen, Julie Andrews, Fred Astaire, Scott Baio',
+  Plot: `A countdown of the "100 greatest musicals" of stage and screen, as vote
+d by the UK public through Channel 4's website and readers of The Mail newspaper
+. Each entry is represented by clips ...`,
+ */
                
                 movies.forEach(element =>  {
-                    let url2 = `http://www.omdbapi.com/?i=tt3896198&apikey=f29c80cb&t=${element}`;
+                    let url2 = `http://www.omdbapi.com/?i=tt3896198&apikey=f29c80cb&t=${coolMovies[element]}`;
                     fetch(url2)
                     .then(response => response.json())
                     .then(data => {
                         console.log(data);
-                        text = text + ` `
-                        
-
-                      
-            }).catch(err => console.log(err))
+                        email.text = email.text  + `Title: ${data.Title} Year: ${data.Year} Rate: ${data.Rated} Actors:${data.Actors} Ploto:${data.Plot}`
+                        console.log(  email.text );            
+            }).then(algo => {
+                console.log(email.text)
+            }).then(data => {
+                
+            mg.messages().send(data, function (error, body) {
+	            console.log(body);
+});
+            }).
+            catch(err => console.log(err))
                 });
             })
             .catch(err => console.log(err))
