5cc1020a08c37838889268bfdb3856591c46d17d
unknown <rafanetow@gmail.com>
2020-05-20 03:39:34 -0700

    email working

--- movie-job.js
+++ movie-job.js
@@ -26,29 +26,12 @@ function run() {
             .then(response => response.json())
             .then(data => {
                 let movies = data[1].typeOfMovies.split(",");
-                console.log("..",coolMovies["actions"])
-                console.log(movies)
-                console.log(data[1].typeOfMovies)
-                console.log(coolMovies);
-                /**Year: '2003',
-  Rated: 'N/A',
-  Released: '26 Dec 2003',
-  Runtime: '270 min',
-  Genre: 'Documentary, Music',
-  Director: 'N/A',
-  Writer: 'Robin Ince, Stewart Williams',
-  Actors: 'Denise Van Outen, Julie Andrews, Fred Astaire, Scott Baio',
-  Plot: `A countdown of the "100 greatest musicals" of stage and screen, as vote
-d by the UK public through Channel 4's website and readers of The Mail newspaper
-. Each entry is represented by clips ...`,
- */
-               
+      
                 movies.forEach(element =>  {
                     let url2 = `http://www.omdbapi.com/?i=tt3896198&apikey=f29c80cb&t=${coolMovies[element]}`;
                     fetch(url2)
                     .then(response => response.json())
                     .then(data => {
-                        console.log(data);
                         email.text = email.text  + `Title: ${data.Title} Year: ${data.Year} Rate: ${data.Rated} Actors:${data.Actors} Ploto:${data.Plot}`
                         console.log(  email.text );            
             }).then(() => {
